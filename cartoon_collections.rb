b408f1620251fa238154647c4129e84b7a1b2539
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-06-24 13:03:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |element|
-    puts array[0]
+ array.each_with_index do |element|
+    array.each_with_index{ |item, index| p "#{index + 1}:#{item}" }
   end
   
     
